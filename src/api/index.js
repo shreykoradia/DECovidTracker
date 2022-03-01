44db49cd5d19cac690de2e57a2851cfce6d098c5
shreykoradia <shrey.koradia@yahoo.com>
2022-03-01 12:51:19 +0530

    done with covid stats

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+const url ='https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) =>{
+    let changeableURL = url;
+    if(country){
+      changeableURL = `${url}/countries/${country}`
+}
+try{
+    const {data} = await axios.get(changeableURL);
+    
+    const modifiedData = {
+        confirmed :data.confirmed,
+        recovered :data.recovered,
+        deaths : data.deaths,
+        lastUpdate :data.lastUpdate
+    }
+    return modifiedData ;
+}
+catch(error){
+  console.log(error)
+}
+}
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export  const fetchCountries = async () =>{
+  
+    try{
+        const {data : {countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country)=>country.name);
+    }
+    catch(error){
+      return error
+    }
+  }
---
+++ src/components/CovidStats/components/index.js
@@ -0,0 +1,3 @@
+export {default as Cards} from './Cards/Cards.jsx'
+export {default as Countrypicker} from './CountryPicker/Countrypicker'
+export {default as Charts} from './Charts/Charts.jsx'
