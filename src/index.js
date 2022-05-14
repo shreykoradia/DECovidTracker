27ce5841d983d396a3b4834d27f860001e4d8e3e
meetkataria <meetkataria03@gmail.com>
2022-05-14 08:50:06 +0530

    Added Travel Details

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import App from './App';
-import Covidstats from './components/CovidStats/Covidstats'
-import Footer from './components/Footer'
-import Review from './components/Team/Review' 
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Covidstats from "./components/CovidStats/Covidstats";
+import Traveldetails from "./components/TravelDetails/Traveldetails";
+import Footer from "./components/Footer";
+import Review from "./components/Team/Review";
 
 ReactDOM.render(
   // <React.StrictMode>
   <BrowserRouter>
-  <Routes>
+    <Routes>
       {/* {routes goes here !! } */}
       <Route path="/" exact element={<App />}></Route>
       <Route path="/covid-stats" element={<Covidstats />}></Route>
+      <Route path="/travel-details" element={<Traveldetails />}></Route>
       <Route path="/footer" element={<Footer />}></Route>
       <Route path="/team" element={<Review />}></Route>
-  </Routes> 
+    </Routes>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
